860ec43d6200279314e88847b506e0946a8d5a6a
clarenceovo <clarenceovo@gmail.com>
2018-10-07 09:20:12 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
+import dateutil.parser
 import matplotlib.pyplot as plt
 import talib
 from sklearn.linear_model import LinearRegression
-import datetime
-import statsmodels
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+#from keras.models import Sequential
+#from keras.layers import Dense
+#from keras.layers import LSTM
 from talib import MA_Type
+
 def getdata():
     global tick
     global cftc
@@ -13,23 +19,47 @@ def getdata():
     cftc= pd.read_csv('C:/Users/LokFung/Desktop/IERGYr4/IEFYP/POCtestdata/CFTC-097741_FO_ALL_CR.csv',usecols=[0,1,2,3,4,5,6,]) # CTFC Report on Future and Option Position Data
     #print(cftc)
     tick=tick.drop_duplicates(keep=False)
+    print('INFO:Tick data is extracted successfully')
     #get date ,time ,open ,high ,low and close data
 
 def candle_baranalysis():
+    global opprice , hiprice , lowprice ,closeprice , dataset, bollupper,bollmiddle,bolllower
     opprice=tick['Open'].astype(float).values
     hiprice=tick['High'].astype(float).values
     lowprice=tick['Low'].astype(float).values
     closeprice=tick['Close'].astype(float).values
     #create different array for pattern analusis using TA lib
     bollupper,bollmiddle,bolllower = talib.BBANDS(closeprice,matype=MA_Type.T3) #make BB Band using Close Price
-
-
+    print('INFO:Candle Parameters is created')
+def technical_analysis():
+    """
+     Create technical parameters using the tick data so that the model can put the param into account
+    Types of Parameters will be calculated:
+    1. MACD
+    2.RSI
+    3.
+    """
+    global macd
+    macd = talib.MACD(opprice) #MACD
+    #print (macd)
+    print ('INFO: Technical Parameters is calculated')
 def dataparsing():
     """
-        1. Normalize the data (optional)
+        1. Create data set
         2. Split the data to test & train dataset (70:30)
 
         """
+    time = tick['Date'] +' '+tick['Time'] #merge date and time
+    time = time.apply (lambda x :dateutil.parser.parse(x)) #Parse the time to fit the transform
+    dataset = pd.DataFrame(data=[time,opprice, hiprice, lowprice, closeprice,bollupper,bollmiddle,bolllower,]).transpose()
+    dataset.columns=['Time','Open','High','Low','Close','BBUpper','BBMiddle','BBLower']
+    dataset.dropna(inplace=True) # move NA dataset
+    values = dataset.values
+    dataencoder = LabelEncoder()
+    values[:,7] = dataencoder.fit_transform(values[:,7])
+    datascaler = MinMaxScaler(feature_range=(0,1))
+    parsed = datascaler.fit_transform(values)
+    print (parsed)
     trainX = tick[:int(0.7 * len(tick))]  # 70% train ,30% Test
     testX = tick[int(0.7 * len(tick)):]
     trainY = tick[:int(0.7 * len(tick))]
@@ -56,10 +86,12 @@ def data_vis():
 if __name__ == '__main__':
     """
     1. Read Data
-    2. Prepare Data
-    3.Create Model 
+    2. Prepare Data and talib data set creation 
+    3. Create Model 
     4. Model evaluation and data visualisation 
     """
     getdata()
     candle_baranalysis()
+    technical_analysis()
+    dataparsing()
     learning()
