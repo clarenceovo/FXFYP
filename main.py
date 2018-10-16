2aee09344afb1316c3da0a43c86ee66c42d95a5a
clarenceovo <clarenceovo@gmail.com>
2018-10-16 18:13:59 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import dateutil.parser
 import matplotlib.pyplot as plt
-import talib
+#import talib
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
@@ -10,8 +10,8 @@ from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-from talib import MA_Type
-
+#from talib import MA_Type
+import math
 def getdata():
     global tick
     global cftc
@@ -29,66 +29,76 @@ def candle_baranalysis():
     lowprice=tick['Low'].astype(float).values
     closeprice=tick['Close'].astype(float).values
     #create different array for pattern analusis using TA lib
-    bollupper,bollmiddle,bolllower = talib.BBANDS(closeprice,matype=MA_Type.T3) #make BB Band using Close Price
+    #bollupper,bollmiddle,bolllower = talib.BBANDS(closeprice,matype=MA_Type.T3) #make BB Band using Close Price
     print('INFO:Candle Parameters is created')
 def technical_analysis():
-    """
-     Create technical parameters using the tick data so that the model can put the param into account
-    Types of Parameters will be calculated:
-    1. MACD
-    2.RSI
-    3.
-    """
-    global macd
-    macd = talib.MACD(opprice) #MACD
-    #print (macd)
+
+   # global macd
+    #macd = talib.MACD(opprice) #MACD
+    ##print (macd)
     print ('INFO: Technical Parameters is calculated')
-def data_timeshift():
+def traceback(data,look_back=1):
+    dataX, dataY = [], []
+    for i in range(len(data) - look_back - 1):
+        a = data[i:(i + look_back)]
+        dataX.append(a)
+        dataY.append(data[i + look_back])
+    return np.array(dataX), np.array(dataY)
 
-    return 0
-def dataparsing():
-    """
-        1. Create data set
-        2. Split the data to test & train dataset (70:30)
-
-        """
+def parsing_learning():
     global trainX ,trainY , testX, testY
     time = tick['Date'] +' '+tick['Time'] #merge date and time
     time = time.apply (lambda x :dateutil.parser.parse(x)) #Parse the time to fit the transform
     time=time.values
-    dataset = pd.DataFrame(data=[time,opprice, hiprice, lowprice, closeprice,bollupper,bollmiddle,bolllower]).transpose()
-    print(dataset)
-    dataset.columns=['Time','Open','High','Low','Close','BBUpper','BBMiddle','BBLower']
+    dataset = pd.DataFrame(data=[opprice, hiprice, lowprice, closeprice]).transpose()
+    dataset.columns=['Open','High','Low','Close']
     dataset.dropna(inplace=True) # move NA dataset
+    #print(dataset)
+    benchmark = dataset['Open'].values
+    #benchmark = benchmark.astype('float')
+    print (benchmark)
+    #testsize = len(benchmark)-trainsize
+    #scaler =MinMaxScaler(0,1)
+    #set = scaler.fit_transform(benchmark)
+    trainsize = len(benchmark) * 0.7
+    train = set[0:trainsize]
+    test = set[trainsize:len(benchmark)]
+    trainX,trainY = traceback(train,1)
+    testX , testY = traceback(test,1)
+    #print(trainX)
+    #print(trainY) #t+1
+    trainX  = np.reshape(trainX,(trainX.shape[0],1,trainX.shape[1]))
+    testX = np.reshape(testX,(testX.shape[0],1,testX.shape[1]))
+    """
     values = dataset.values
     dataencoder = LabelEncoder()
-    values[:,7] = dataencoder.fit_transform(values[:,7])
+    values[:,6] = dataencoder.fit_transform(values[:,6])
     datascaler = MinMaxScaler(feature_range=(0,1))
     parsed = datascaler.fit_transform(values)
-
-    train = parsed[:int(0.7 * len(parsed)),:]  # 70% train ,30% Test
-    test = parsed[int(0.7 * len(parsed)):,:]
-    trainX ,trainY = train[:,:-1],train[:,-1]
-    testX, testY = test[:, :-1], test[:, -1]
-    trainX=trainX.reshape((trainX.shape[0],1,trainX.shape[1]))
-    testX = testX.reshape((testX.shape[0], 1, testX.shape[1]))
-    print (testX)
-def learning():
-  global result
-  model = Sequential()
-  model.add(LSTM(50,input_shape=(trainX.shape[1],trainX.shape[2]))) #50 Nodes
-  model.add(Dense(1,activation='sigmoid')) # Add one dense layer
-  model.compile(loss='mae',optimizer='rmsprop')
-  supervised = model.fit(trainX,trainY,epochs=100,batch_size=20,validation_data=(testX,testY) ,verbose=2 ,shuffle=False)
-
-def data_vis():
     """
-    1. Plot the graph (actual and prediction)
-    2. Save it to gif
-
-    """
-    return 0
-
+    #train = parsed[:int(0.7 * len(parsed)),:]  # 70% train ,30% Test
+    #test = parsed[int(0.7 * len(parsed)):,:]
+    #trainX ,trainY = train[:,:-1],train[:,-1]
+    #testX, testY = test[:, :-1], test[:, -1]
+    #trainX=trainX.reshape((trainX.shape[0],1,trainX.shape[1]))
+    #testX = testX.reshape((testX.shape[0], 1, testX.shape[1]))
+    print('INFO:The dataset is parsed')
+    global result , prediction
+    model = Sequential()
+    model.add(LSTM(4,input_shape=(1,1)))
+    model.add(Dense(1))# Add one dense layer
+    model.compile(loss='mse',optimizer='rmsprop')
+    model.fit(trainX,trainY,epochs=100,batch_size=100,verbose=2 )
+    trainprediction = model.predict(testX)
+    testprediction = model.predict(testX)
+    #trainprediction = scaler.inverse_transform(trainprediction)
+    #trainY=scaler.inverse_transform([trainY])
+    #testprediction=scaler.inverse_transform(testprediction)
+    #testY=scaler.inverse_transform([testY])
+    trainScore = math.sqrt(mean_squared_error(trainY[0], trainprediction[:, 0]))
+    print('Train Score: %.2f RMSE' % (trainScore))
+    testScore = math.sqrt(mean_squared_error(testY[0], testprediction[:, 0]))
+    print('Test Score: %.2f RMSE' % (testScore))
 ##Main Loop##
 if __name__ == '__main__':
     """
@@ -100,5 +110,4 @@ if __name__ == '__main__':
     getdata()
     candle_baranalysis()
    # technical_analysis()
-    dataparsing()
-    learning()
+    parsing_learning()
